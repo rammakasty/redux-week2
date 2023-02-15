003c6bb66caed42ae0d8b5e31adf3cc6cc7943de
choi ha young <gkdudk15@naver.com>
2023-02-16 00:48:59 +0900

    리덕스 사용 기능구현완료

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,86 @@ import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
+const App = () => {
+    const dispatch = useDispatch();
+
+    const todoState = useSelector((state) => {
+        return state.todos;
+    }); // [ ] < 여기에다가 {}를 넣는다.
+
+    const [todo, setTodo] = useState({
+        id: Date.now(),
+        title: '',
+        isDone: false,
+    });
+
+    const handleChange = ({ target: { value: title } }) => {
+        setTodo((pre) => ({
+            ...pre,
+            title,
+        }));
+    };
+    return (
+        <StContainer>
+            <div>
+                <Stlabel>제목</Stlabel>
+                <StInput type="text" value={todo.title} onChange={handleChange} />
+                <button
+                    onClick={() => {
+                        dispatch({
+                            type: '추가하기',
+                            payload: todo,
+                        });
+                    }}
+                >
+                    Todo 추가하기
+                </button>
+            </div>
+            <StTitle>Todo!</StTitle>
+            <div>
+                <h1>Works</h1>
+                {todoState
+                    .filter((item) => item.isDone === false)
+                    .map((item) => (
+                        <StTodoItem key={item.id}>
+                            {item.title}{' '}
+                            <button
+                                onClick={() => {
+                                    dispatch({ type: '삭제하기', payload: item.id });
+                                }}
+                            >
+                                삭제
+                            </button>
+                            <button onClick={() => dispatch({ type: '업데이트', payload: item })}>
+                                완료
+                            </button>
+                        </StTodoItem>
+                    ))}
+            </div>
+            <div>
+                <h1>Done</h1>
+                {todoState
+                    .filter((item) => item.isDone === true)
+                    .map((item) => (
+                        <StTodoItem key={item.id}>
+                            {item.title}{' '}
+                            <button
+                                onClick={() => {
+                                    dispatch({ type: '삭제하기', payload: item.id });
+                                }}
+                            >
+                                삭제
+                            </button>
+                            <button onClick={() => dispatch({ type: '업데이트', payload: item })}>
+                                취소
+                            </button>
+                        </StTodoItem>
+                    ))}
+            </div>
+        </StContainer>
+    );
+};
+
 const StContainer = styled.section`
     display: flex;
     flex-direction: column;
@@ -30,51 +110,5 @@ const StTitle = styled.h1`
     font-size: 24px;
     margin-bottom: 16px;
 `;
-
-const App = () => {
-    const dispatch = useDispatch();
-
-    const todos = useSelector((state) => {
-        console.log('todos', state.todos.todos);
-        return state.todos.todos;
-    }); // [ ] < 여기에다가 {}를 넣는다.
-
-    const [todo, setTodo] = useState({
-        id: new Date(),
-        title: '',
-    });
-
-    const handleChange = ({ target: { value: title } }) => {
-        setTodo((pre) => ({
-            ...pre,
-            title,
-        }));
-    };
-    return (
-        <StContainer>
-            <div>
-                <Stlabel>제목</Stlabel>
-                <StInput type="text" value={todo.title} onChange={handleChange} />
-                <button
-                    onClick={() => {
-                        dispatch({
-                            type: '추가하기',
-                            payload: todo,
-                        });
-                    }}
-                >
-                    Todo 추가하기
-                </button>
-            </div>
-
-            <StTitle>Todo!</StTitle>
-            <div>
-                {todos.map((todo) => (
-                    <StTodoItem key={todo.id}>{todo.title}</StTodoItem>
-                ))}
-            </div>
-        </StContainer>
-    );
-};
 
 export default App;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,30 +1,32 @@
 // 액션 타입
 
 // 액션 크리에이터
-
 // 초기값
-const initialState = {
-    todos: [
-        {
-            id: 1,
-            title: '리액트는 재밌다',
-        },
-        {
-            id: 2,
-            title: '코딩은 재밌어요',
-        },
-    ],
-};
+// array [{},{}]
+// return [{기존에 있던 값},업{데이트된 값}]
+const initialState = [
+    {
+        id: 1,
+        title: '리액트는 재밌다',
+        isDone: false,
+    },
+    {
+        id: 2,
+        title: '코딩은 재밌어요',
+        isDone: true,
+    },
+];
 
-// 리듀서 = 함수다.
+// 리듀서 = 함수다
 const todosReducer = (state = initialState, action) => {
     switch (action.type) {
         case '추가하기':
-            return {
-                ...state,
-                todos: state.todos.concat(action.payload),
-            };
-
+            return [...state, action.payload];
+        case '삭제하기':
+            return state.filter((item) => item.id !== action.payload);
+        case '업데이트':
+            action.payload.isDone = !action.payload.isDone;
+            return state.map((item) => (item.id === action.payload.id ? action.payload : item));
         default:
             return state;
     }
